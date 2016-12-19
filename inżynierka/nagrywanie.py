5b2c4dc7f3730659444fb8878fbab1a1385819b9
malinowydyplom <malinowydyplom@gmail.com>
2016-12-19 16:18:12 +0100

    Update nagrywanie.py

--- inżynierka/nagrywanie.py
+++ inżynierka/nagrywanie.py
@@ -6,11 +6,12 @@ camera = PiCamera()
 
 camera.resolution = (720, 400)
 camera.framerate = 32
-
+name = raw_input("Wprowadz nazwe pliku: ")
+name=str(name)
 
 
 camera.start_preview()
-camera.start_recording('/home/pi/video.h264')
+camera.start_recording("/home/pi/"+name+".h264")
 sleep(15)
 camera.stop_recording()
 camera.stop_preview()
